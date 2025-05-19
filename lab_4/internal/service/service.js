d211e0383cad93b78ffa2d8da3c0151845a3c597
111 <sin.leonid@gmail.com>
2025-05-19 03:48:02 +0300

    lab4 done

---
+++ lab_4/internal/service/service.js
@@ -0,0 +1,37 @@
+const { IDCounter } = require('../db/counter');
+
+class Service {
+    constructor(repository) {
+      this.repo = repository;
+      this.idCounter = new IDCounter();
+    }
+  
+    findTemplates(filters = {}) {
+      if (filters.id) {
+        return this.repo.findById(filters.id);
+      }
+      return this.repo.find(filters);
+    }
+  
+    addTemplate(data) {
+      
+      const newId = this.idCounter.getNextId();
+      const templateData = {
+        ...data,
+        id: newId
+      };
+      return this.repo.insert(templateData);
+    }
+  
+    updateTemplate(id, updatedData) {
+      return this.repo.update(id, updatedData);
+    }
+  
+    deleteTemplate(id) {
+      return this.repo.delete(id);
+    }
+}
+
+module.exports={
+    Service,
+}
